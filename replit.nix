3d9a1561099a3b70c585a9f031fa102cb758a805
Swayam <108215141+Swayam2245@users.noreply.github.com>
2022-06-29 11:03:09 +0000

    Changes Are OP

---
+++ replit.nix
@@ -0,0 +1,16 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python39Full
+    pkgs.python39Packages.pip.out
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      pkgs.glib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python39Full}/bin/python3.9";
+    LANG = "en_US.UTF-8";
+  };
+}
